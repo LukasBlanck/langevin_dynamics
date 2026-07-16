65f038782d7c64e88da52ff1135d81f2a761951b
Lukas Blanck <lukas.blanck02@gmail.com>
2026-07-16 17:46:08 +0200

    added CUDA_CHECK file

---
+++ src/GPU/cuda_check.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#define CUDA_CHECK(call)                                                                                     \
+    do {                                                                                                     \
+        cudaError_t error = (call);                                                                          \
+        if (error != cudaSuccess) {                                                                          \
+            std::cerr << "CUDA error at " << __FILE__ << ":" << __LINE__ << ": "                             \
+                      << cudaGetErrorString(error) << '\n';                                                  \
+            std::exit(EXIT_FAILURE);                                                                         \
+        }                                                                                                    \
+    } while (0)
